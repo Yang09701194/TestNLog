b3ffcfa9ac7bf788ab02ac29738735e8a8f7ed4b
yang <Yang.Jian@grandsys.com>
2018-06-07 17:16:10 +0800

    Test NLog archieve by date success!!!

---
+++ TestNLog/TestNLog/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                var config = new NLog.Config.LoggingConfiguration();
+                var logger = NLog.LogManager.GetLogger("AppLogDate");
+
+
+                //for (int i = 0; i < 10000; i++)
+                //{
+                    if(logger.IsErrorEnabled)
+                        logger.Error(DateTime.Now+"\n");
+                //}
+
+                Console.WriteLine("pause");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+
+
+        }
+    }
+}
